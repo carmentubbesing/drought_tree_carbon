9aff75e4a220ca07e331a3925ce223b6e5288bbd
Carmen Tubbesing <ctubbesing@berkeley.edu>
2018-04-04 15:20:24 -0700

    run non-sierra counties

--- code/counties/GET_RESULTS_CA_COUNTIES.R
+++ code/counties/GET_RESULTS_CA_COUNTIES.R
@@ -9,13 +9,6 @@ source("aggregate_pixels.R")
 
 load(file = "../data/CA_counties.Rdata")
 
-# this section is for if you want to exclude counties in the Southern Sierra
-# nrow(counties)
-# SScounties <- list.dirs("~/drought_tree_carbon/sierra_regions/data/counties/", recursive = F, full.names = F)
-# length(SScounties)
-# counties <- subset(counties, !counties@data$NAME %in% as.character(SScounties))
-# nrow(counties)
-
 to <- "~/drought_tree_carbon/drought_tree_carbon/data/active_unit/"
 
 counties@data$NAME
@@ -25,25 +18,16 @@ setwd("../../sierra_regions/data/sierra_regions/")
 sierra_counties <- readOGR(dsn = "counties_Sierra", layer = "counties_Sierra")
 plot(sierra_counties)
 
-# this section is for if you want to exclude counties in the Southern Sierra
-# nrow(counties)
-# SScounties <- list.dirs("~/drought_tree_carbon/sierra_regions/data/counties/", recursive = F, full.names = F)
-# length(SScounties)
-# counties <- subset(counties, !counties@data$NAME %in% as.character(SScounties))
-# nrow(counties)
-
 to <- "~/drought_tree_carbon/drought_tree_carbon/data/active_unit/"
 
 counties@data$NAME
-counties <- subset(counties, counties@data$NAME %in% sierra_counties@data$NAME_PCASE)
-counties@data$NAME
-counties <- subset(counties, !counties@data$NAME %in% c("Los Angeles", "San Bernardino"))
+counties <- subset(counties, !counties@data$NAME %in% sierra_counties@data$NAME_PCASE | counties@data$NAME %in% c("Los Angeles", "San Bernardino"))
 counties@data$NAME
 plot(counties)
-plot(counties[counties@data$NAME == "Shasta",], add = T, col = "pink")
-counties@data$NAME[13]
 
-for(i in 13:length(counties)){
+
+for(i in 19:length(counties)){
+  print(i)
   # Remove county from folder
   active_county <- list.dirs("~/drought_tree_carbon/drought_tree_carbon/data/active_unit/", recursive = F)
   unlink(active_county, recursive = T)
